1c1890cf0be16e1a97a7b733f8a3a4adab8d31b4
marcosrmartin <marcos_2.001@hotmail.com>
2024-09-06 09:34:25 +0000

    añadido sshpass a la instalación #50

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 
-sudo apt-get install python3 python3-pip pkg-config libcairo2-dev python3-poetry docker.io firewalld libgirepository1.0-dev -y
+sudo apt-get install python3 python3-pip pkg-config libcairo2-dev python3-poetry docker.io firewalld libgirepository1.0-dev sshpass -y
 
 poetry install
